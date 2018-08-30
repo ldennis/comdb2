710ededb0d53cfcfd4e72f9d07157d2f5efc5286
Lingzhi Deng <ldeng33@bloomberg.net>
2018-08-30 16:19:22 -0400

    fix bug caused by IX_NOTFOUND being equal to IX_DUP

--- db/record.c
+++ db/record.c
@@ -2772,8 +2772,12 @@ int upd_new_record(struct ireq *iq, void *trans, unsigned long long oldgenid,
         }
 
         /* remap delete not found to retry */
-        if (rc == IX_NOTFND && verify_retry)
-            rc = RC_INTERNAL_RETRY;
+        if (rc == IX_NOTFND) {
+            if (verify_retry)
+                rc = RC_INTERNAL_RETRY;
+            else
+                rc = ERR_VERIFY;
+        }
 
         if (rc != 0) {
             if (rc != IX_NOTFND)
@@ -3003,8 +3007,12 @@ int del_new_record(struct ireq *iq, void *trans, unsigned long long genid,
         }
 
         /* remap delete not found to retry */
-        if (rc == IX_NOTFND && verify_retry)
-            rc = RC_INTERNAL_RETRY;
+        if (rc == IX_NOTFND) {
+            if (verify_retry)
+                rc = RC_INTERNAL_RETRY;
+            else
+                rc = ERR_VERIFY;
+        }
 
         if (rc != 0) {
             retrc = rc;
